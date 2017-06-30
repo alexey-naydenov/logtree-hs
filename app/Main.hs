237c7d29035b997b53e62dc99dc0143dffad283a
Alexey Naydenov <alexey.naydenov@linux.com>
2017-06-30 09:34:52 +0300

    Move library around

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import qualified LogTree.Lib as LTL
 
 main :: IO ()
-main = someFunc
+main = LTL.someFunc
--- src/LogTree/Lib.hs
+++ src/LogTree/Lib.hs
@@ -1,4 +1,4 @@
-module Lib
+module LogTree.Lib
     ( someFunc
     ) where
 
